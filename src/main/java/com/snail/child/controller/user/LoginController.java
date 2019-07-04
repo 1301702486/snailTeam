fd2c0fdb6e9d9395b4283bc475a1ab9dac8595f8
1301702486 <1301702486@qq.com>
2019-07-04 11:34:51 +0800

    login controller

---
+++ src/main/java/com/snail/child/controller/user/LoginController.java
@@ -0,0 +1,58 @@
+package com.snail.child.controller.user;
+
+import com.snail.child.model.Result;
+import com.snail.child.model.User;
+import com.snail.child.service.user.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Author: 陈一
+ * Date: 2019/6/27
+ * Description: LoginController
+ */
+@RestController
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+//    /**
+//     * 跳转到登录界面
+//     *
+//     * @param model
+//     * @return
+//     */
+//    @RequestMapping("/toLogin")
+//    public String toLogin(Model model) {
+//        List<User> users = service.getAllUsers();
+//        int userCount = users.size();
+//        model.addAttribute("userCount", userCount);
+//        return "user/login";
+//    }
+
+
+    @RequestMapping("/login")
+    public Result login(@RequestParam("emailAddr") String emailAddr,
+                      @RequestParam("password") String password,
+                      HttpServletRequest request) throws IOException {
+
+        Result result = loginService.login(emailAddr, password);
+        if (result.getCode().equals(0)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("emailAddr", emailAddr);
+            session.setAttribute("password", password);
+            System.out.println(session.getId());
+        }
+        return result;
+    }
+
+}
